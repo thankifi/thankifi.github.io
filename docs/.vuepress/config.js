d2c818a66e3bb0409b554873158cfa986f095162
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-16 15:29:01 +0300

    feat: update config.js for vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,18 @@
 module.exports = {
     base: '/',
     title: 'Thankifi',
-    description: 'Thankifi is a free (as in speech) API that makes it easy for you to express gratitude. Be grateful!'
+    description: 'Thankifi is a free (as in speech) API that makes it easy for you to express gratitude. Be grateful!',
+    themeConfig: {
+        // logo: '/images/logo_transparent.png',
+        navbar: [
+            {
+                text: 'API Reference',
+                link: 'https://api.thanki.fi',
+            },
+        ],
+        repo: 'thankifi'
+    },
+    plugins: [
+        '@vuepress/back-to-top'
+    ]
 }
